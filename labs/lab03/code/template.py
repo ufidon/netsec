b22008efabcb54f8f7d569f317a63fbe1688340e
ufidon <ufidon@gmail.com>
2022-01-24 20:39:21 -0600

    lab03/04 created

--- labs/lab03/code/template.py
+++ labs/lab03/code/template.py
@@ -36,18 +36,4 @@ send(pkt, iface="eth0", verbose=0)
 #  Launching the attack automatically
 # check lecture code
 
-# Task 3.1 ----------------------------------------------------
-# Launching the attack manually
-#!/usr/bin/env python3
-from scapy.all import *
 
-ip = IP(src="@@@@", dst="@@@@")
-tcp = TCP(sport=@@, dport=@@, flags="A", seq=@@@@, ack=@@@@)
-data = "@@@@"
-pkt = ip/tcp/data
-ls(pkt)
-send(pkt, iface="eth0", verbose=0)
-
-# Task 2.2 ----------------------------------------------------
-#  Launching the attack automatically
-# check lecture code
--- labs/lab04/code/template.py
+++ labs/lab04/code/template.py
@@ -1,33 +1,21 @@
-# retype following the code in the lab manual to save your time
+# the code exerpted from the lab manual to save your time
 
-# Task 1.1 ----------------------------------------------------
+# Task 3.1 ----------------------------------------------------
+# Launching the attack manually
 #!/usr/bin/env python3
 from scapy.all import *
 
-def print_pkt(pkt):
-  pkt.show()
+ip = IP(src="@@@@", dst="@@@@")
+tcp = TCP(sport=@@, dport=@@, flags="A", seq=@@@@, ack=@@@@)
+data = "@@@@"
+pkt = ip/tcp/data
+ls(pkt)
+send(pkt, iface="eth0", verbose=0)
 
-# The interface can be found with
-# 'docker network ls' in the VM
-# or 'ifconfig' in the containner
-pkt = sniff(iface='br-c93733e9f913', filter='icmp', prn=print_pkt)  
+# Task 3.2 ----------------------------------------------------
+#  Launching the attack automatically
+# check lecture code
 
-# Task 1.2 ----------------------------------------------------
-from scapy.all import *
-a = IP()
-a.dst = '10.0.2.3'
-b = ICMP()
-p = a/b
-send(p)
-
-ls(a)
-
-# Task 1.3 ----------------------------------------------------
-a = IP()
-a.dst = '1.2.3.4'
-a.ttl = 3
-b = ICMP()
-send(a/b)
-
-# Task 1.4 ----------------------------------------------------
-# Lookup the code in the lecture
+# Task 4 ----------------------------------------------------
+#  Launching the attack automatically
+# check lecture code
